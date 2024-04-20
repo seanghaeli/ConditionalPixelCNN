314139f1c42291a4e6492b440f78dcf556cd2f26
Sean Ghaeli <seanghaeli@gmail.com>
2024-04-20 00:29:48 -0700

    Large model hyperparameters

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 python pcnn_train.py \
---batch_size 16 \
+--batch_size 32 \
 --sample_batch_size 192 \
 --sampling_interval 100 \
 --save_interval 100 \
 --dataset cpen455 \
---nr_resnet 1 \
---nr_filters 40 \
---nr_logistic_mix 5 \
+--nr_resnet 5 \
+--nr_filters 160 \
+--nr_logistic_mix 10 \
 --lr_decay 0.999995 \
 --max_epochs 500 \
 --en_wandb True \
