3a28606d67c515313a362ef00b12d1234f58a476
Sean Ghaeli <seanghaeli@gmail.com>
2024-04-19 20:51:27 -0700

    Increase sampling batch size for T4 GPU compatibility

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 python pcnn_train.py \
 --batch_size 16 \
---sample_batch_size 50 \
---sampling_interval 50 \
---save_interval 50 \
+--sample_batch_size 192 \
+--sampling_interval 100 \
+--save_interval 100 \
 --dataset cpen455 \
 --nr_resnet 1 \
 --nr_filters 40 \
