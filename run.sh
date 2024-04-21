89361712480f9efe6773981e9997eaa59a38cadf
Sean Ghaeli <seanghaeli@gmail.com>
2024-04-20 17:54:48 -0700

    Lean run for testing

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 python pcnn_train.py \
---batch_size 32 \
+--batch_size 1 \
 --sample_batch_size 16 \
 --sampling_interval 10 \
 --save_interval 5 \
 --dataset cpen455_complex_embeddings \
---nr_resnet 5 \
---nr_filters 160 \
---nr_logistic_mix 10 \
+--nr_resnet 1 \
+--nr_filters 1 \
+--nr_logistic_mix 1 \
 --lr_decay 0.999995 \
 --max_epochs 500 \
 --en_wandb True \
