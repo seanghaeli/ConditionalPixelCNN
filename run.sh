dbc0328d06e6d103dc2c9305efcafd508e64de62
Sean Ghaeli <seanghaeli@gmail.com>
2024-04-21 22:42:43 -0700

    Larger hyper params

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@ python pcnn_train.py \
 --batch_size 16 \
 --sample_batch_size 16 \
 --sampling_interval 100 \
---save_interval 5 \
---dataset cpen455_multi_complex_embeddings_3 \
---nr_resnet 4 \
---nr_filters 100 \
---nr_logistic_mix 8 \
+--save_interval 2 \
+--dataset cpen455_multi_complex_embeddings_4 \
+--nr_resnet 10 \
+--nr_filters 160 \
+--nr_logistic_mix 10 \
 --lr_decay 0.999995 \
 --max_epochs 50 \
 --en_wandb True \
